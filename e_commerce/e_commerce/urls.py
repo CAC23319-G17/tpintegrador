39a1ab4c4d58d58127a4144cd04f146a6e1b6131
lucas marques de paiva <lucasmarques6@hotmail.com>
2023-04-14 16:47:25 -0300

    Creacion de todas las aplicaciones, urls, setting

--- e_commerce/e_commerce/urls.py
+++ e_commerce/e_commerce/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
+    path('carrito', include('carrito.urls')),
+    path('pedidos', include('pedidos.urls')),
+    path('productos', include('productos.urls')),
+    path('userApp', include('userApp.urls')),
+    
 ]
