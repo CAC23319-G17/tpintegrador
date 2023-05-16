599cd4299e1eb5f6c9c32eae96ce2df27b7de46c
dcazabat <dacazabat@gmail.com>
2023-05-15 21:33:38 -0300

    Avance 20230515

--- e_commerce/productos/models.py
+++ e_commerce/productos/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    state = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    state = models.BooleanField(default=True)
+    description = models.TextField()
+    urlimage = models.URLField()
+    rating = models.FloatField()
+    idCategory = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.title
